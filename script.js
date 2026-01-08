60721f940f4901f7412cdaa8a2ea7059b49e27bc
tanmayNapte <tanmaynapte232@gmail.com>
2026-01-08 13:43:52 +0530

    Deleted old portfolio files

--- script.js
+++
@@ -1,9 +0,0 @@
-
-// Smooth scroll
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener("click", e => {
-    e.preventDefault();
-    document.querySelector(link.getAttribute("href"))
-      .scrollIntoView({ behavior: "smooth" });
-  });
-});
